712f7188dc439e0aa78a71cc30196cf86d6cb9ca
Anostosia117 <jigsa@yandex.ru>
2022-03-28 22:55:43 +0300

    задание 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Задание 1
+
+        int num1 = 127;
+        var num2 = 456;
+        byte num3 = 3;
+        short num4 = 234;
+        long num5 = 4357L;
+        float num6 = 4.34f;
+        double num7 = 34.5224564;
+        char num8 = 4267;
+        boolean isTrue = num1 < num2;
     }
 }
