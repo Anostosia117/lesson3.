a1de83b3cd5881e0a2ed32450a4c7bf37ec94971
Anostosia117 <jigsa@yandex.ru>
2022-03-28 22:56:25 +0300

    задание 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,15 @@ public class Main {
         double num7 = 34.5224564;
         char num8 = 4267;
         boolean isTrue = num1 < num2;
+
+        // Задание 2
+
+        float boxerFirst = 78.2f;
+        float boxerSecond = 82.7f;
+        float sumWeight = boxerFirst + boxerSecond;
+        float weightDifference = boxerSecond % boxerFirst;
+        System.out.println("The total weight of athletes " + sumWeight + " kg");
+        System.out.println("Athlete weight difference " + weightDifference + " kg");
+
     }
 }
